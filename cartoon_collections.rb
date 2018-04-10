67f21e6b6f33812ab29435f71ed7b3d29d0282a3
Saige Leslie <saigeleslie@gmail.com>
2018-04-10 06:15:48 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,9 @@ end
 
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.collect do |calls|
-    calls.split.split
+    calls.split.collect do |call|
+      call.split
+    end
   end
 end
 
