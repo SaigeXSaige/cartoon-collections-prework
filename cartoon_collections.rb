6cd732f986cc4d1215dffdb4411cde8e30b2a707
Saige Leslie <saigeleslie@gmail.com>
2018-04-10 06:30:05 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,5 +20,5 @@ def find_the_cheese(strings)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   strings.find do |cheese?|
-    cheese? == 
+    cheese? == cheese_types
 end
