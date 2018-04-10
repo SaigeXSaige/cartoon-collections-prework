62b8174e4f0af71bd211459db1b424a57315ff3e
Saige Leslie <saigeleslie@gmail.com>
2018-04-10 06:09:30 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,8 @@ def roll_call_dwarves(dwarf_names)
 end
 
 def summon_captain_planet(planeteer_calls)
-  # Your code here
+  planeteer_calls.collect do |calls|
+    calls.split
 end
 
 def long_planeteer_calls# code an argument here
