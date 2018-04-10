f7fb9298e6aa7335d5d3848cd768931681031db6
Saige Leslie <saigeleslie@gmail.com>
2018-04-10 06:01:31 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,6 @@
 def roll_call_dwarves(dwarf_names)
-  # Your code here
+  dwarf_names.each_with_index do |names, index|
+    puts (index + 1) + names
 end
 
 def summon_captain_planet# code an argument here
